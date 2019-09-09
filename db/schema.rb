b9a9e7eb44e7e34dc61abd8088b5dfcd8519fb78
Anthony <anthonywan00@gmail.com>
2019-09-09 13:23:23 -0400

    removed age column from users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_135902) do
+ActiveRecord::Schema.define(version: 2019_09_09_172136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_09_09_135902) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
